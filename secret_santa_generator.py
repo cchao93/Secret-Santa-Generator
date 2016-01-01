a65d5740dd4d85c7e1976a29d64eb4cf7fcb264e
Victor Chao <chia-chi.chao@tufts.edu>
2016-01-02 00:10:43 +0800

    created participant list

--- secret_santa_generator.py
+++ secret_santa_generator.py
@@ -1,25 +1,40 @@
 import smtplib
+from collections import defaultdict
 
-fromaddr = "vcchao93@gmail.com"
-toaddrs = ["victorchao666@gmail.com"]
+def createParticipantList(filename):
+	participant_list = list()
+	txt_file = open(filename, "r")
+	for line in txt_file:
+		participant_list.append(tuple(line[:-1].split(", ")))
+	return participant_list
 
-message = """From: Victor Chao <vcchao93@gmail.com>
-To: To Person <victorchao666@gmail.com>
-Subject: SMTP e-mail test
+def sendMails():
+	from_addr = "vcchao93@gmail.com"
+	to_addrs = ["victorchao666@gmail.com"]
 
-This is a test e-mail message.
-"""
+	message = """From: Victor Chao <vcchao93@gmail.com>
+	To: To Person <victorchao666@gmail.com>
+	Subject: SMTP e-mail test
 
-username = "vcchao93"
-password = "PASSWORD"
+	This is a test e-mail message.
+	"""
 
-try:
-	server = smtplib.SMTP("smtp.gmail.com:587")
-	#server.ehlo()
-	server.starttls()
-	server.login(username, password)
-	server.sendmail(fromaddr, toaddrs, message)
-	server.quit()
-	print "Successfully sent email"
-except smtplib.SMTPException:
-	print "Error: unable to send email"
+	username = "vcchao93"
+	password = "PASSWORD"
+
+	try:
+		server = smtplib.SMTP("smtp.gmail.com:587")
+		#server.ehlo()
+		server.starttls()
+		server.login(username, password)
+		server.sendmail(from_addr, to_addrs, message)
+		server.quit()
+		print "Successfully sent email"
+	except smtplib.SMTPException:
+		print "Error: unable to send email"
+
+def main():
+	createParticipantList("participants.dat")
+
+if __name__ == "__main__": 
+    main()
