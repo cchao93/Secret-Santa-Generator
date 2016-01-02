67c95b213bb353c7a4446b2bf8d83c1ddf9ce03e
Victor Chao <chia-chi.chao@tufts.edu>
2016-01-03 01:58:33 +0800

    program is now functional

--- secret_santa_generator.py
+++ secret_santa_generator.py
@@ -3,6 +3,14 @@ from collections import defaultdict
 import copy
 import random
 
+def promptForOrganizerInfo():
+	name = raw_input("Thank you for using the Secret Santa Generator.\nWhat is your name?\n")
+	email = raw_input("What is your email address?\n")
+	filename = raw_input("What is the name of the file containing information about the participants?\n")
+	price_limit = raw_input("What is the maximum expenditure limit on a present?\n")
+	organizer_info = (name, email)
+	return organizer_info, price_limit, filename
+
 def createParticipantList(filename):
 	participant_list = list()
 	txt_file = open(filename, "r")
@@ -34,34 +42,44 @@ def pairParticipants(participant_list):
 	print participant_pairs
 	return participant_pairs
 
-def sendMails():
-	from_addr = "vcchao93@gmail.com"
-	to_addrs = ["victorchao666@gmail.com"]
-
-	message = """From: Victor Chao <vcchao93@gmail.com>
-	To: To Person <victorchao666@gmail.com>
-	Subject: SMTP e-mail test
-
-	This is a test e-mail message.
-	"""
-
+def sendMailsToParticipants(organizer, price_limit, participant_pairs):
 	username = "vcchao93"
-	password = "PASSWORD"
-
+	password = "victor15334c"
 	try:
 		server = smtplib.SMTP("smtp.gmail.com:587")
 		#server.ehlo()
 		server.starttls()
 		server.login(username, password)
+		print "Successfully connected to SMTP server"
+	except smtplib.SMTPException:
+		print "Error: unable to connect to SMTP server"
+	for giver, receiver in participant_pairs.iteritems():
+		sendMail(server, organizer, price_limit, giver, receiver)
+	server.quit()
+
+def sendMail(server, organizer, price_limit, giver, receiver):
+	from_addr = organizer[1]
+	to_addrs = giver[1]
+	subject = "Secret Santa Notification"
+	body = """Dear %s,\n\nThank you for signing up to participate \
+in a Secret Santa organized by %s. You've been assigned %s, and the \
+maximum expenditure limit on a present is $%s. Please contact \
+%s if you have any questions.\n\nBest,\nSecret Santa Generator
+	""" % (giver[0], organizer[0], receiver[0], price_limit, organizer[0])
+	message = """From: %s <%s>\nTo: %s <%s>\nSubject: %s\n\n%s
+	""" % (organizer[0], organizer[1], giver[0], giver[1], subject, body)
+
+	try:
 		server.sendmail(from_addr, to_addrs, message)
-		server.quit()
 		print "Successfully sent email"
 	except smtplib.SMTPException:
 		print "Error: unable to send email"
 
 def main():
-	participant_list = createParticipantList("participants.dat")
+	organizer, price_limit, filename = promptForOrganizerInfo()
+	participant_list = createParticipantList(filename)
 	participant_pairs = pairParticipants(participant_list)
+	sendMailsToParticipants(organizer, price_limit, participant_pairs)
 
 if __name__ == "__main__": 
     main()
